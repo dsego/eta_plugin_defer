22276fb75227645ab9c7c706eea1ad3610d5609d
Davorin Šego <davorin.sego@gmail.com>
2021-01-27 09:18:55 +0100

    Write some plugin code

---
+++ demo.js
@@ -0,0 +1,22 @@
+import * as Eta from "https://deno.land/x/eta@v1.12.1/mod.ts"
+import MyEtaPlugin from "./plugin.js"
+
+// RUN
+// deno run --unstable --allow-read demo.js
+
+
+// Eta templates
+Eta.configure({ 
+  async: true,
+  views: Deno.cwd(),
+  plugins: [MyEtaPlugin],
+})
+
+console.time("Time to render")
+
+const html = await Eta.renderFile("page.eta");
+
+console.log(html)
+
+console.timeEnd("Time to render")
+
---
+++ plugin.js
@@ -0,0 +1,33 @@
+const regex = /^\s*deferFile\s*\(\s*["']([^]*)["'],?([^]*)\)$/
+
+export default {
+  processAST(buffer, env) {
+    const promises = [];
+    const result = [];
+    for (let entry of buffer) {
+      if (entry.val) {
+        const val = entry.val.trim()
+        if (regex.test(val)) {
+          const match = regex.exec(val);
+          const filename = match[1];
+          const paramStr = match[2].trim();
+          promises.push({ filename, paramStr })
+        }
+      }
+      result.push(entry)
+    }
+
+    if (promises.length) {
+      const promiseCalls = promises.map(({ filename, paramStr }) =>
+        `\nE.includeFile("${filename}", ${paramStr || null})`
+      ).join(",")
+
+      const topline =
+        `const __resolved = await Promise.all([${promiseCalls}]);
+        const deferFile = () => __resolved.shift();`
+
+      result.unshift({ t: "e", val: topline })
+    }
+    return result
+  }
+}
